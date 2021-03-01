93cd18e02f72f4da8a0c3ec8b7e5370fcf7d1ad4
Demianchuk <vanohahj5555@gmail.com>
2021-03-01 19:28:51 +0200

    Added new data table on the admin panel and changed routing

--- FreshFishWebsite/wwwroot/js/site.js
+++ FreshFishWebsite/wwwroot/js/site.js
@@ -1,1 +1,14 @@
-﻿
+﻿$(document).ready( function () {
+    $('.basic-table').DataTable({
+        lengthChange: false,
+        "info": false,
+        "language": {
+            "lengthMenu": "Відобразити _MENU_ елементів на сторінці",
+            "zeroRecords": "Нічого не знайдено - вибачте",
+            "info": "Показана сторінка _PAGE_ з _PAGES_",
+            "infoEmpty": "No records available",
+            searchPlaceholder: "Пошук",
+            search: "_INPUT_",
+        }
+    });
+} );
