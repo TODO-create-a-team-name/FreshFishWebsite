9012a78c565b462f98a783c373ac2efd58644dbb
Vasya Lesiuk <lenono526@gmail.com>
2021-03-04 20:39:30 +0200

    added unworked validation

--- FreshFishWebsite/ViewModels/CreateStorageViewModel.cs
+++ FreshFishWebsite/ViewModels/CreateStorageViewModel.cs
@@ -1,10 +1,13 @@
 ﻿
+using Microsoft.AspNetCore.Mvc;
+
 namespace FreshFishWebsite.ViewModels
 {
     public class CreateStorageViewModel
     {
         public int StorageNumber { get; set; }
         public string Address { get; set; }
+        [Remote(action: "CheckEmailAsync", controller: "Storage")]
         public string StorageAdminEmail { get; set; }
     }
 }
