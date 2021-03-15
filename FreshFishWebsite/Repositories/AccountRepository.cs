ab925c2fa3f01371fea27bfb874e15909609fcfc
Serge <sanichace@gmail.com>
2021-03-16 00:49:29 +0200

    closed #64

--- FreshFishWebsite/Repositories/AccountRepository.cs
+++ FreshFishWebsite/Repositories/AccountRepository.cs
@@ -1,68 +1,15 @@
-﻿
-
-using FreshFishWebsite.Interfaces;
-using FreshFishWebsite.Models;
-using FreshFishWebsite.ViewModels;
+﻿using FreshFishWebsite.Interfaces.Registering;
 using Microsoft.AspNetCore.Identity;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace FreshFishWebsite.Repositories
 {
-    public class AccountRepository<T> : IAccountRepository<T> where T : class
+    public class AccountRepository : IAccountRepository
     {
-        private readonly UserManager<User> _userManager;
-        private readonly FreshFishDbContext _context;
-        public AccountRepository(UserManager<User> userManager, FreshFishDbContext context)
+        public async Task<IEnumerable<IdentityError>> RegisterClaim(IRegisterClaim claim)
         {
-            _userManager = userManager;
-            _context = context;
-        }
-
-        public async Task<IEnumerable<IdentityError>> RegisterUser(RegisterStorageWorker model)
-        {
-            var storage = _context.Storages.FirstOrDefault(s => s.Id == model.StorageId);
-            IdentityResult result = new();
-            if(typeof(T) == typeof(StorageAdmin))
-            {
-                StorageAdmin user = new()
-                {
-                    Email = model.Email,
-                    UserName = model.Email,
-                    Name = model.Name,
-                    EmailConfirmed = true,
-                    Usersurname = model.Surname,
-                    Company = model.Company,
-                    CompanyAddress = model.CompanyAddress,
-                    Storage = storage
-                };
-                result = await _userManager.CreateAsync((StorageAdmin)(object)user, model.Password);
-                await _userManager.AddToRoleAsync((StorageAdmin)(object)user, "AdminAssistant");
-                storage.StorageAdmin = (StorageAdmin)(object)user;
-                _context.Storages.Update(storage);
-            }
-            else if(typeof(T) == typeof(Driver))
-            {
-                Driver user = new()
-                {
-                    Email = model.Email,
-                    UserName = model.Email,
-                    Name = model.Name,
-                    EmailConfirmed = true,
-                    Usersurname = model.Surname,
-                    Company = model.Company,
-                    CompanyAddress = model.CompanyAddress,
-                    Storage = storage
-                };
-                result = await _userManager.CreateAsync((Driver)(object)user, model.Password);
-                await _userManager.AddToRoleAsync((Driver)(object)user, "Driver");
-                storage.Drivers.Add((Driver)(object)user);
-                _context.Storages.Update(storage);
-            }
-            await _context.SaveChangesAsync();
-            return result.Errors;
+            return await claim.Register();
         }
     }
 }
---
+++ FreshFishWebsite/Services/ClaimsRegister/StorageAdminRegistration.cs
@@ -0,0 +1,46 @@
+﻿using FreshFishWebsite.Interfaces.Registering;
+using FreshFishWebsite.Models;
+using FreshFishWebsite.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreshFishWebsite.Services.ClaimsRegistration
+{
+    public class StorageAdminRegistration : IRegisterClaim
+    {
+        private readonly FreshFishDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RegisterStorageWorker _model;
+        public StorageAdminRegistration(
+            RegisterStorageWorker model,
+            FreshFishDbContext context,
+            UserManager<User> userManager)
+        {
+            _model = model;
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IEnumerable<IdentityError>> Register()
+        {
+            var storage = _context.Storages.FirstOrDefault(s => s.Id == _model.StorageId);
+            StorageAdmin storageAdmin = new()
+            {
+                Email = _model.Email,
+                UserName = _model.Email,
+                Name = _model.Name,
+                EmailConfirmed = true,
+                Usersurname = _model.Surname,
+                Company = _model.Company,
+                CompanyAddress = _model.CompanyAddress,
+                Storage = storage
+            };
+            var result = await _userManager.CreateAsync(storageAdmin, _model.Password);
+            await _userManager.AddToRoleAsync(storageAdmin, "AdminAssistant");
+            storage.StorageAdmin = storageAdmin;
+            _context.Storages.Update(storage);
+            return result.Errors;
+        }
+    }
+}
